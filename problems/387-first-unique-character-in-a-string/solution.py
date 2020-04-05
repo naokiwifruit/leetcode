c14a1d2d3656a93dac88cf34ebc682d911d867d2
naokiwifruit <41097442+naokiwifruit@users.noreply.github.com>
2020-04-05 22:28:53 +0900

    solved 387-first-unique-character-in-a-string

---
+++ problems/387-first-unique-character-in-a-string/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        """
+        :type s: str
+        :rtype: int
+        """
+        count = collections.Counter(s)
+        
+        for idx, ch in enumerate(s):
+            if count[ch] == 1:
+                return idx
+        return -1
