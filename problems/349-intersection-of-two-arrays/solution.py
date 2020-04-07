5275ae06272bfaed32c93df6c1d11c3f109916a4
naokiwifruit <41097442+naokiwifruit@users.noreply.github.com>
2020-04-07 22:06:34 +0900

    solved 349-intersection-of-two-arrays

---
+++ problems/349-intersection-of-two-arrays/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """  
+        set1 = set(nums1)
+        set2 = set(nums2)
+        return list(set2 & set1)
+        
+# Time complexity: O(n * m)
+# Space complexity:　O(n * m)
